a5dc0e0f8c923b28d8d159acc059e9665f8d5eb0
angelvaldesmx <angelmills982@gmail.com>
2025-06-27 00:45:57 -0600

    Actualizar addContactToMailchimp.js

--- netlify/functions/addContactToMailchimp.js
+++ netlify/functions/addContactToMailchimp.js
@@ -2,11 +2,13 @@ const fetch = require("node-fetch");
 const mandrill = require("mandrill-api/mandrill");
 const admin = require("firebase-admin");
 
-// === Inicializar Firebase Admin con credenciales desde ENV ===
+const credentials = JSON.parse(
+  Buffer.from(process.env.FIREBASE_CREDENTIALS_BASE64, "base64").toString("utf8")
+);
+
 if (!admin.apps.length) {
-  const credentials = JSON.parse(process.env.FIREBASE_CREDENTIALS_JSON);
   admin.initializeApp({
-    credential: admin.credential.cert(credentials)
+    credential: admin.credential.cert(credentials),
   });
 }
 
