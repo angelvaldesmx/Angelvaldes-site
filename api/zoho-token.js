88152201181b98eb2acac4d60b4c556b60d93b0d
angelvaldesmx <angelmills982@gmail.com>
2025-06-28 02:58:44 -0600

    Create zoho-token.js

---
+++ api/zoho-token.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Método no permitido' });
+  }
+
+  const params = new URLSearchParams({
+    grant_type: 'authorization_code',
+    client_id: process.env.ZOHO_CLIENT_ID,
+    client_secret: process.env.ZOHO_CLIENT_SECRET,
+    redirect_uri: 'https://angelvaldesmx.qzz.io/kit',
+    code: req.query.code,
+  });
+
+  const response = await fetch('https://accounts.zoho.com/oauth/v2/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: params,
+  });
+
+  const result = await response.json();
+  res.status(200).json(result);
+}
