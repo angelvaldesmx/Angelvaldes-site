62c1158b7c33746ff279a1ce8a90d74aadb2d86d
angelvaldesmx <angelmills982@gmail.com>
2025-06-28 11:14:25 -0600

    Actualizar cargar-aliases.js

--- pages/api/cargar-aliases.js
+++ pages/api/cargar-aliases.js
@@ -17,7 +17,7 @@ async function initFirebase() {
 }
 
 export default async function handler(req, res) {
-  if (req.method !== 'GET') {
+  if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Método no permitido' });
   }
 
